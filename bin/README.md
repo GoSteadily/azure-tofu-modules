e27a8ebcd3a6600197258dfa89381bdd1dd37d35
Dwayne Crooks <me@dwaynecrooks.com>
2025-12-11 12:23:22 -0400

    Add recreate-vm

--- bin/README.md
+++ bin/README.md
@@ -11,3 +11,4 @@ Useful utilities for managing the infrastructure.
 
 - [`save-keys node`](./bin/save-keys) - Save the public and private keys of a given node to separate files.
 - [`with-pg-env pg-command`](./bin/with-pg-env) - Run a PostgreSQL command within the proper database context.
+- [`recreate-vm`](./bin/recreate-vm) - Destroy and recreate a given virtual machine.
--- nix/scripts.nix
+++ nix/scripts.nix
@@ -26,11 +26,15 @@ stdenv.mkDerivation {
     install -m644 lib.sh "$out/bin/lib.sh"
     install -m755 save-keys "$out/bin/unwrapped-save-keys"
     install -m755 with-pg-env "$out/bin/unwrapped-with-pg-env"
+    install -m755 recreate-vm "$out/bin/unwrapped-recreate-vm"
 
     makeWrapper "$out/bin/unwrapped-save-keys" "$out/bin/save-keys" \
       --prefix PATH : ${lib.makeBinPath commonPackages}
 
     makeWrapper "$out/bin/unwrapped-with-pg-env" "$out/bin/with-pg-env" \
+      --prefix PATH : ${lib.makeBinPath commonPackages}
+
+    makeWrapper "$out/bin/unwrapped-recreate-vm" "$out/bin/recreate-vm" \
       --prefix PATH : ${lib.makeBinPath commonPackages}
   '';
 }
